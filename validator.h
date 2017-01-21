7e066f8857469ba345fbcff3d5d877bb44c418c2
Marcin <martin.kania@gmail.com>
2017-01-21 13:58:59 +0100

    Validator function declarations

--- validator.h
+++ validator.h
@@ -1,6 +1,11 @@
 #ifndef VALIDATOR_H
 #define VALIDATOR_H
 
+#include "graph.h"
+
+bool validateCircuitEquation(CircuitGraph* graph);
+
+CircuitGraph* mergeCircuitGraphs(CircuitGraph* graph1, CircuitGraph* graph2);
 
 
 #endif // VALIDATOR_H
